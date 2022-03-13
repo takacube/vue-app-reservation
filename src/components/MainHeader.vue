e5f6510135c442de5239f7ca1b85ea0d898d56d4
taka <shimakuratakanao@gmail.com>
2022-03-13 13:54:36 +0900

    add header

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="title">
+        <h1>じらふ</h1>
+        <h1>{{ todaydate }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainHeader',
+    data() {
+        return {
+            todaydate: ""
+        }
+    },
+    mounted: function(){
+        const today = new Date()
+        const thisYear = today.getFullYear()
+        const nextMonth = today.getMonth() + 2
+        this.todaydate = `${thisYear}年${nextMonth}月分の予約`
+    }
+}
+</script>
+<style>
+    #title {
+        color: black;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import HeaderSet from './HeaderSet.vue'
+import MainBody from './MainBody.vue'
 createApp(App).mount('#app')
+createApp(HeaderSet).mount('#header')
+createApp(MainBody).mount('#main')
