6863d586c0aa5173801e4f82834f0d66905fbe4d
taka <shimakuratakanao@gmail.com>
2022-04-08 00:29:24 +0900

    create login_page

--- src/views/Description.vue
+++ src/views/Description.vue
@@ -1,17 +1,54 @@
 <template>
-    <div class="desc">
-        <h1>This is an description page</h1>
+    <div class="ui placeholder segment">
+        <div class="ui two column very relaxed stackable grid">
+            <div class="column">
+                <div class="ui form">
+                    <div class="field">
+                        <label>名前</label>
+                        <div class="ui left icon input">
+                            <input type="text" placeholder="なまえ" v-model="name">
+                            <i class="user icon"></i>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label>パスワード</label>
+                        <div class="ui left icon input">
+                            <input type="password" v-model="password">
+                            <i class="lock icon"></i>
+                        </div>
+                    </div>
+                    <button class="ui blue submit button" type="submit" @click="handle()">ログイン</button>
+                </div>
+            </div>
+            <div class="middle aligned column">
+                <div class="ui big button">
+                    <i class="signup icon"></i>
+                    初めての方
+                </div>
+            </div>
+        </div>
+        <div class="ui vertical divider">
+            Or
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
         name: "Description",
+        data() {
+            return {
+                date: "xxx",
+                info: "test"
+            }
+        },
+        methods: {
+            handle: function () {
+                this.$router.push('./calendar')
+            }
+        }
     }
 </script>
-
 <style>
-    .desc h1{
-        color: red;
-    }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,32 +1,54 @@
 <template>
-    <div class="login">
-        <div><h1>ログインしてください。</h1></div>
-        <div class="form-item">
-            <label for="email">Email</label>
-            <input
-                id="email"
-                autocomplete="off"
-                type="text"
-                v-model="email"
-            >
+    <div class="ui placeholder segment">
+        <div class="ui two column very relaxed stackable grid">
+            <div class="column">
+                <div class="ui form">
+                    <div class="field">
+                        <label>名前</label>
+                        <div class="ui left icon input">
+                            <input type="text" placeholder="なまえ" v-model="name">
+                            <i class="user icon"></i>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label>パスワード</label>
+                        <div class="ui left icon input">
+                            <input type="password" v-model="password">
+                            <i class="lock icon"></i>
+                        </div>
+                    </div>
+                    <button class="ui blue submit button" type="submit" @click="handle()">ログイン</button>
+                </div>
+            </div>
+            <div class="middle aligned column">
+                <div class="ui big button">
+                    <i class="signup icon"></i>
+                    初めての方
+                </div>
+            </div>
         </div>
-        <div class="form-item">
-            <label for="password">Password</label>
-            <input
-                id="password"
-                autocomplete="off"
-                type="password"
-                v-model="password"
-            >
-        </div>
-        <div class="form-item">
-            <button class="button" @click="handle()">Login</button>
+        <div class="ui vertical divider">
+            Or
         </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
         name: "Login",
+        data() {
+            return {
+                date: "xxx",
+                info: "test"
+            }
+        },
+        methods: {
+            handle: function () {
+                this.$router.push('./calendar')
+            }
+        }
     }
 </script>
+<style>
+</style>
