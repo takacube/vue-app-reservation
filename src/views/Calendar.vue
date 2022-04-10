eb183c6b9ec986541f80eb156686e07cc54fdb3a
taka <shimakuratakanao@gmail.com>
2022-04-10 14:23:23 +0900

    create calendar

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,9 +1,93 @@
 <template>
     <h2>Calendar{{ currentDate }}</h2>
-    <div v-for="(week, index) in calendars" :key="index" style="display:flex" class="ui buttons">
-        <form v-for="(day, index) in week" :key="index">
-            <button v-bind:id="day.date" class="ui blue basic button" @click=showModal()>{{ day.date }}</button>
-        </form>
+        <div class="week-title">
+            <div v-for="column in weekList" :key="column.item" class="each-week">
+                {{column.item}}
+            </div>
+        </div>
+        <div class="calendar-ui">
+            <div v-for="(week, index) in calendars" :key="index" class="ui buttons capcher-calendar">
+                <form class="form-ui" v-for="(day, index) in week" :key="index">
+                    <button v-bind:id="{ active: isActive}" class="ui button button-1" @click="modalOpen = day.date;">{{ day.date }}</button>
+                </form>
+            </div>
+        </div>
+        <h1>{{reservationList}}</h1>
+    <teleport to="body">
+        <div v-if="modalOpen" class="modal">
+            <div class="modalButton">
+                <h3>{{modalOpen}}日</h3>
+                <form class="ui form">
+                    <div class="field">
+                        <label>開始時間</label>
+                        <input v-model="startTime" type="time" name="first-name">
+                    </div>
+                    <div class="field">
+                        <label>終了時間</label>
+                        <input v-model="endTime" type="time" name="first-name">
+                    </div>
+                    <div class="field">
+                        <label>目的地</label>
+                        <input v-model="destination" type="text" name="last-name" placeholder="目的地を入力してください">
+                    </div>
+                    <div class="field">
+                        <label>ご要望があれば入力ください</label>
+                        <textarea v-model="description" name="kanso" rows="4" cols="40"></textarea>
+                    </div>
+
+                    <button class="ui button" @click="modalOpen=false">閉じる</button>
+                    <button class="blue ui button" type="submit" @click="addToList(modalOpen)">　完了　</button>
+                </form>
+            </div>
+        </div>
+    </teleport>
+    <teleport to="body" :scrollable=true>
+        <div v-if="checkModalOpen" class="modal">
+            <div class="modalButton scroll">
+                <form class="ui form">
+                    <a class="ui teal tag label">5/9</a>
+                    <div class="field">
+                        <label>開始時間</label>
+                        <input type="time" name="first-name" value="09:00">
+                        <label>終了時間</label>
+                        <input type="time" name="first-name" value="15:00">
+                        <label>目的地</label>
+                        <input type="text" name="last-name" placeholder="目的地を入力してください" value="サンシャイン水族館">
+                        <label>ご要望があれば入力ください</label>
+                        <textarea name="kanso" rows="4" cols="40"></textarea>
+                    </div>
+                    <a class="ui teal tag label">5/19</a>
+                    <div class="field">
+                        <label>開始時間</label>
+                        <input type="time" name="first-name" value="12:00">
+                        <label>終了時間</label>
+                        <input type="time" name="first-name" value="19:00">
+                        <label>目的地</label>
+                        <input type="text" name="last-name" placeholder="目的地を入力してください" value="向ヶ丘公園">
+                        <label>ご要望があれば入力ください</label>
+                        <textarea name="kanso" rows="4" cols="40"></textarea>
+                    </div>
+                    <a class="ui teal tag label">5/27</a>
+                    <div class="field">
+                        <label>開始時間</label>
+                        <input type="time" name="first-name" value="12:00">
+                        <label>終了時間</label>
+                        <input type="time" name="first-name" value="18:00">
+                        <label>目的地</label>
+                        <input type="text" name="last-name" placeholder="目的地を入力してください" value="ディズニーランド">
+                        <label>ご要望があれば入力ください</label>
+                        <textarea name="kanso" rows="4" cols="40"></textarea>
+                    </div>
+                    <button class="ui button" @click="checkModalOpen=false">戻る</button>
+                    <button class="blue ui button" type="submit">　予約を確定　</button>
+                </form>
+            </div>
+        </div>
+    </teleport>
+    <div class="submit-button-div">
+        <button class="massive blue ui button submit-button" @click="checkModalOpen = true;">
+            予約確認へ
+        </button>
     </div>
 </template>
 <script>
@@ -13,6 +97,12 @@ export default {
     data() {
         return {
             currentDate: moment(),
+            modalOpen: false,
+            weekList: [{item: "日"}, {item: '月'}, {item: '火'}, {item: '水'}, {item: '木'}, {item: '金'}, {item: '土'}],
+            pushedDate: [],
+            isActive: true,
+            checkModalOpen: false,
+            reservationList: [],
         };
     },
     methods: {
@@ -48,6 +138,18 @@ export default {
         },
         showModal() {
             this.$router.push('./')
+        },
+        addToList(selectedDate) {
+            const list = { date: selectedDate, startTime: this.startTime, endTime: this.endTime, destination: this.destination, description: this.description}
+            let existedList = []
+            if(this.$cookies.get("reservationAllList").length > 0){
+                existedList = JSON.parse(this.$cookies.get("reservationAllList"))
+                existedList.push(list)
+            } else {
+                existedList.push(list)
+            }
+            this.$cookies.set("reservationAllList", JSON.stringify(existedList))
+            /*this.reservationList.push(list)*/
         }
     },
     computed: {
@@ -57,3 +159,95 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+    .modal {
+        position: absolute;
+        top: 0; right: 0; bottom: 0; left: 0;
+        background-color: rgba(0,0,0,.5);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .modal div {
+        display: fix;
+        justify-content: center;
+        width: 400px;
+        padding: 20px;
+        border-radius: 20px 20px 20px 20px;
+    }
+    .modalButton {
+        background: rgb(255, 255, 255);
+        align-items: center;
+    }
+    .field{
+        max-width: 100%;
+    }
+    .capcher-calendar {
+        background: rgb(223, 223, 223);
+        margin: 0 auto;
+        width: 80%;
+        display: flex;
+        justify-content: space-between;
+    }
+    .calendar-ui {
+        max-width: 100%;
+        width: 100%;
+        /*padding: 1.5rem 2rem;*/
+        padding-right: 0px;
+        padding-left: 0px;
+        margin: 0 auto;
+        text-align: justify;
+    }
+    .button-1 {
+        background-color: rgb(234, 234, 234);
+        color: white;
+        margin: 0%;
+        width: 100%;
+        padding-top: 30px;
+        padding-bottom: 30px;
+        color: rgb(0, 0, 0);
+    }
+    .form-ui {
+        margin: 0%;
+        padding: 0;
+        width: 100%;
+    }
+    .week-title {
+        display: flex;
+        margin: 0 auto;
+        width: 80%;
+        padding: 0;
+        justify-content: space-between;
+    }
+    .each-week {
+        text-align: center;
+        width: 100%;
+        padding-top: 30px;
+        padding-bottom: 30px;
+        background-color: rgb(153, 239, 236);
+    }
+    #calendar-1 {
+        background-color: red;
+    }
+    .active {
+        background-color: red;
+    }
+    .submit-button-div {
+        width: 80%;
+        margin: 0 auto;
+        text-align: right;
+    }
+    .submit-button {
+        margin-top: 20px;
+        text-align: right;
+        margin-left: auto;
+        margin-right: 0;
+    }
+    .scroll{
+        height: 80%;
+        overflow: auto;
+    }
+</style>
